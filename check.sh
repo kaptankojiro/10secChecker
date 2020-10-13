f6bf072bcde905454b4c4a1ad0c7911228bdd3e0
Fatih <kaptankojiro@gmail.com>
2020-10-13 12:22:46 +0300

    Create check.sh

---
+++ check.sh
@@ -0,0 +1,29 @@
+while true ; do
+
+clear   
+
+uptime -p
+echo ""
+
+ips=($(hostname -I))
+for ip in "${ips[@]}"
+do
+
+    echo "IP Address:" $ip
+done
+
+echo ""
+
+nmcli device status
+
+echo ""
+echo "Cpu Status":
+ps -axro pcpu | awk '{sum+=$1} END {print sum}'
+
+echo  ""
+echo "RAM Status"
+free | awk '/Mem/{printf("used: %.2f%"), $3/$2*100} /buffers\/cache/{printf(", buffers: %.2f%"), $4/($3+$4)*100} /Swap/{printf(", swap: %.2f%"), $3/$2*100}'
+
+sleep 5
+
+done
